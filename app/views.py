edcd50e8987fb0adfc24565258af29d53ec4a845
uroshini <uroshini123@gmail.com>
2023-11-20 23:00:22 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def roshini(request):
+    return render(request,'roshini.html')
+def bhargavi(request):
+    return render(request,'bhargavi.html')
